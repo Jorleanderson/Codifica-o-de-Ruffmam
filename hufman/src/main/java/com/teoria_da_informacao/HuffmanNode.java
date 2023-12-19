813a7666eef082fe1f550c2b547abf44a6f910e9
Jorleanderson <jorleandersonmoraes@gmail.com>
2023-12-19 10:02:54 -0300

    Codificacao de Ruffmam 1.0

---
+++ hufman/src/main/java/com/teoria_da_informacao/HuffmanNode.java
@@ -0,0 +1,13 @@
+package com.teoria_da_informacao;
+/*
+ * Classe de um nó da Árvore 
+ */
+class HuffmanNode extends HuffmanTree {
+    public final HuffmanTree left, right; // sub-árvores
+ 
+    public HuffmanNode(HuffmanTree l, HuffmanTree r) {
+        super(l.frequency + r.frequency);
+        left = l;
+        right = r;
+    }
+}
